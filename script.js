61b53dc3504c6a831d1fd27da4826b4111fd7a36
Jyotasana17 <jyotasana05jan07@gmail.com>
2025-07-07 00:38:02 +0530

    modify and update newly added files

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+javascriptcity_costs = {
+    "mumbai": 3100,
+    "delhi": 2700,
+    "bangalore": 2500,
+    "chennai": 2400,
+    "hyderabad": 2200,
+    "pune": 2400,
+    "kolkata": 2000,
+    "indore": 1800,
+    "nagpur": 1700,
+    "mysuru": 1600
+};
